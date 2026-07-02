043ba1e6dd9645d20b3f35fae5201bf7166cc87b
ahsngll <241038034@stu.gedik.edu.tr>
2026-07-02 11:13:02 +0300

    sayi tahmin projesi yapildi

---
+++ yeniprjj/Form2.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace yeniprjj
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstSonuclar = new ListBox();
+            label2 = new Label();
+            txtTahmin = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lstSonuclar
+            // 
+            lstSonuclar.BackColor = Color.White;
+            lstSonuclar.FormattingEnabled = true;
+            lstSonuclar.Location = new Point(245, 49);
+            lstSonuclar.Name = "lstSonuclar";
+            lstSonuclar.Size = new Size(244, 264);
+            lstSonuclar.TabIndex = 9;
+            lstSonuclar.SelectedIndexChanged += lstSonuclar_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Silver;
+            label2.Location = new Point(55, 250);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 20);
+            label2.TabIndex = 8;
+            label2.Text = "kalan tahmin hakkı";
+            label2.Click += label2_Click;
+            // 
+            // txtTahmin
+            // 
+            txtTahmin.BackColor = Color.LavenderBlush;
+            txtTahmin.Location = new Point(55, 67);
+            txtTahmin.Name = "txtTahmin";
+            txtTahmin.Size = new Size(120, 27);
+            txtTahmin.TabIndex = 7;
+            txtTahmin.TextChanged += txtTahmin_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Location = new Point(55, 118);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 57);
+            button1.TabIndex = 6;
+            button1.Text = "try";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkGray;
+            label1.Location = new Point(104, 293);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 20);
+            label1.TabIndex = 5;
+            label1.Text = "10";
+            label1.Click += label1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 377);
+            Controls.Add(lstSonuclar);
+            Controls.Add(label2);
+            Controls.Add(txtTahmin);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lstSonuclar;
+        private Label label2;
+        private TextBox txtTahmin;
+        private Button button1;
+        private Label label1;
+    }
+}
---
+++ yeniprjj/Form2.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace yeniprjj
+{
+    public partial class Form2 : Form
+    {
+        int tahminEdilecekSayi = 0;
+        int hamleSayisi = 10;
+        int denemesayi = 10;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+                Random rnd = new Random();
+            tahminEdilecekSayi = rnd.Next(1000, 9999);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string tahmin = txtTahmin.Text;
+            int x = int.Parse(tahmin);
+            try
+            {
+                int sayı = int.Parse(txtTahmin.Text);
+            }
+            catch
+            {
+                MessageBox.Show("harf girilemez");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tahmin))
+            {
+                MessageBox.Show("boş bırakılamaz");
+                return;
+            }
+            else if (tahmin.Length != 4)
+            {
+                MessageBox.Show("4 basamaklı bir sayı gir!");
+                return;
+            }
+
+            else if (tahmin[0] == '0')
+            {
+                MessageBox.Show("0 ile başlayamaz!");
+                return;
+            }
+
+            denemesayi = denemesayi - 1; ;
+            label1.Text = denemesayi.ToString();
+            if (denemesayi <= 0)
+            {
+                button1.Enabled = false;
+                // button1.Visible = false;
+            }
+            /*
+            int fark = tahminEdilecekSayi - x;
+
+            
+            string m = fark switch
+            {
+                0 => "Tebrikler, bildiniz! ",
+                > 0 and <= 15 => "Yukarı! Neredeyse bildin ",
+                < 0 and >= -15 => "Aşağı! Neredeyse bildin ",
+                > 15 and <= 60 => "Yukarı! Çok yaklaştın ",
+                < -15 and >= -60 => "Aşağı! Çok yaklaştın ",
+                > 60 and <= 200 => "Yukarı! Yaklaştın ",
+                < -60 and >= -200 => "Aşağı! Yaklaştın ",
+                > 200 => "Yukarı! Çok uzaksın ",
+                _ => "Aşağı! Çok uzaksın " 
+            };
+
+            lstSonuclar.Items.Add(m);
+
+            lstSonuclar.Items.Add($"{hamleSayisi}.Tahmin: {string.Join("", tahmin)}");
+            
+            txtTahmin.Clear();
+            */
+
+
+            int fark = tahminEdilecekSayi - x;
+
+            string m = fark switch
+            {
+                0 => "Tebrikler, bildin",
+
+                > 0 and <= 20 => "Yukarı! Neredeyse bildin ",
+                < 0 and >= -20 => "Aşağı! Neredeyse bildin ",
+
+                > 20 and <= 40 => "Yukarı! Çok yaklaştın ",
+                < -20 and >= -40 => "Aşağı! Çok yaklaştın ",
+
+                > 40 and <= 60 => "Yukarı! Yaklaştın ",
+                < -40 and >= -60 => "Aşağı! Yaklaştın ",
+
+                > 60 and <= 80 => "Yukarı! Biraz yaklaştın ",
+                < -60 and >= -80 => "Aşağı! Biraz yaklaştın ",
+
+                > 80 and <= 100 => "Yukarı! Uzak değilsin ",
+                < -80 and >= -100 => "Aşağı! Uzak değilsin ",
+
+                > 100 and <= 150 => "Yukarı! Uzaksın ",
+                < -100 and >= -150 => "Aşağı! Uzaksın ",
+
+                > 150 and <= 200 => "Yukarı! Çok uzaksın ",
+                < -150 and >= -200 => "Aşağı! Çok uzaksın ",
+
+                > 200 => "Yukarı! Bayağı uzaksınız ",
+                _ => "Aşağı! Bayağı uzaksınız"
+            };
+
+                txtTahmin.Clear();
+
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstSonuclar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTahmin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
